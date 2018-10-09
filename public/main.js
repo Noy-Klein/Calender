d5a37a648e20d4f2bea6dec31aa67c11a01f70d2
unknown <danny.michaelov@gmail.com>
2018-10-09 18:17:51 +0300

    addEventsHandler func done

--- public/main.js
+++ public/main.js
@@ -1,30 +1,4 @@
 
-// Get the modal
-var modal = document.getElementById('myModal');
-    
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-/*
 import EventsRepository from './events-repository'
 import EventsRenderer from './events-renderer'
 import EventsHandler from './events-handler'
@@ -33,9 +7,9 @@ let eventsRepository = new EventsRepository();
 let eventsRenderer = new EventsRenderer();
 let eventsHandler = new EventsHandler(eventsRepository, eventsRenderer)
 
-var d = new Date();
-var m = d.getMonth(); 
-*/
+// var d = new Date();
+// var m = d.getMonth(); 
+
 /*
 $(document).on('click', '#next', function(){
 
