b894d9a70c5113b9c5e2de23e304fcd41af19b5c
unknown <danny.michaelov@gmail.com>
2018-10-09 13:04:36 +0300

    added classes files

---
+++ public/events-handler.js
@@ -0,0 +1,9 @@
+class EventsHandler {
+    constructor(eventsRepository, eventsHandler) {
+        this.eventsRepository = eventsRepository;
+        this.eventsHandler = eventsHandler;
+        //inputs here
+    }
+}
+
+export default EventsHandler;
