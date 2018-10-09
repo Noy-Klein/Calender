2942080448f13251bb05bae08ca74b651ae75bb3
ofrizadok <ofrizz@walla.com>
2018-10-09 15:16:35 +0300

    html&cssWork

--- public/events-handler.js
+++ public/events-handler.js
@@ -4,6 +4,59 @@ class EventsHandler {
         this.eventsRenderer = eventsRenderer;
         //inputs here
     }
+
+    nextMonth(){
+        
+    }
+
+    lastMonth(){
+        
+    }
+    addEventDetails(){
+        
+        $('#todaysEvents').on("click", function () {
+            let title = $(".title-input").val()
+            let name = $(".name-input").val()
+            let time = $(".time-input").val()
+            let date = $(".date-input").val()
+            let image = $(".image-input").val()
+
+            postUnicornInfo(title,day,image,time,name)
+        })
+
+    }
+// Get the modal
+var modal = document.getElementById('myModal');
+    
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+    showDayEvents(){
+
+    }
+    showSpecialEvents(){
+
+    }
 }
 
 export default EventsHandler;
+
--- public/main.js
+++ public/main.js
@@ -24,6 +24,7 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+/*
 import EventsRepository from './events-repository'
 import EventsRenderer from './events-renderer'
 import EventsHandler from './events-handler'
@@ -31,3 +32,48 @@ import EventsHandler from './events-handler'
 let eventsRepository = new EventsRepository();
 let eventsRenderer = new EventsRenderer();
 let eventsHandler = new EventsHandler(eventsRepository, eventsRenderer)
+
+var d = new Date();
+var m = d.getMonth(); 
+*/
+/*
+$(document).on('click', '#next', function(){
+
+    var str = m + 1;
+    var str1 = y;
+
+
+    if (str=="" )
+      {
+        document.getElementById("calendar_preview").innerHTML="";
+          return;
+      } 
+      if (window.XMLHttpRequest)
+      {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+      }
+      else
+      {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+
+      xmlhttp.onreadystatechange=function()
+      {
+
+        if(document.getElementById("calendar_preview") != null) {
+
+
+          if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+              document.getElementById("calendar_preview").innerHTML=xmlhttp.responseText;
+            }
+        }
+      }
+
+    var url = calendar_vars.plugin_url + "?month=" + str +"&"+"year="+str1;
+
+    xmlhttp.open("GET",url,true);
+    xmlhttp.send(); 
+});
+
+*/
