6fcad06e24e37149acc9fe086554700071086e68
unknown <danny.michaelov@gmail.com>
2018-10-10 15:40:59 +0300

    updated add event forms

--- public/events-handler.js
+++ public/events-handler.js
@@ -64,21 +64,18 @@ class EventsHandler {
             let day = $(".day").val();
             let month = $('.model-content').find('.month').val();
             console.log(month);
-            let year = $(".Year").val();
             let time = $(".time").val();
             let location = $(".location").val();
             let image = $(".image").val();
-            if (title == '' || day == '' || month == '' || year == '') {
+            if (title == '' || day == '' || month == '') {
                 if (title == "") { alert("Please enter title") }
                 if (day == "") { alert("Please enter day") }
                 if (month == "") { alert("Please enter month") }
-                if (year == "") { alert("Please enter year") }
             } else {
                 title = $(".title").val();
                 day = $(".day").val();
                 month = $('.model-content').find('.month').val();
-                year = $(".Year").val();
-                this.eventsRepository.addEvent(title, day, month, year, time, location, image).then(() => {
+                this.eventsRepository.addEvent(title, day, month, time, location, image).then(() => {
                     this.eventsRenderer.renderEvents(this.eventsRepository.events, this.cm);
                     const model = $("#mymodel");
                     model.css('display', 'none');
@@ -86,7 +83,6 @@ class EventsHandler {
                 $(".title").val('');
                 $(".day").val('');
                 $(".month").val('');
-                $(".Year").val('');
                 $(".time").val('');
                 $(".location").val('');
                 $(".image").val('');
@@ -95,60 +91,6 @@ class EventsHandler {
         })
 
     }
-    // Get the model
-    // var model = document.getElementById('mymodel');
-
-    // Get the button that opens the model
-    // var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the model
-    // var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the model 
-    // btn.onclick = function() {
-    //     model.style.display = "block";
-    // }
-
-
-
-    // When the user clicks on <span> (x), close the model
-    // span.onclick = function() {
-    //     model.style.display = "none";
-    // }
-
-
-
-    // // When the user clicks anywhere outside of the model, close it
-    // window.onclick = function(event) {
-    //     if (event.target == model) {
-    //         model.style.display = "none";
-    //     }
-    // }
-    // showDayEvents() {
-    //     $("").on('click', function() {
-
-    //     })
-    // }
-
-    // showSpecialEvents() {
-    //     $("").on('click', function(){
-
 }
+
 export default EventsHandler;
-
-
-// var dateControl = document.querySelector('input[type="date"]');
-// dateControl.value = '2018-06-01';
-
-// var calendar = new Y.Calendar({
-//     contentBox: "#mycalendar",
-//     width:'100%',
-//     showPrevMonth: true,
-//     showNextMonth: true,
-//     date: new Date()}).render();
-
-
-//       $("#1 tr1").click(function(){
-//         window.location = "example.html";
-//       });
-
